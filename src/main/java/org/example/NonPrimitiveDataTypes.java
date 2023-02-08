6b81d0fe9ca626ac231bc75a43073f6825688c71
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-02-08 18:36:28 +0530

    primitive and non-primitive data types

---
+++ src/main/java/org/example/NonPrimitiveDataTypes.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class NonPrimitiveDataTypes {
+  public static void main(String[] args) {
+    String str1 = "askdjalskdja"; // string will reuse the same object
+    System.out.println(str1);
+
+    str1 = "altjwlrq";
+//    str1[0] = 'd'; // error
+    System.out.println(str1);
+
+    for (int i = 0; i < str1.length(); i++) {
+      System.out.println(str1.charAt(i));
+    }
+
+    String str2 = new String("lasdjlasdjlasd"); // with new, everytime we call it, it creates a new object
+    System.out.println(str2);
+
+    int arr[];
+    arr = new int[2];
+    arr[0] = 0;
+    arr[1] = 1;
+    System.out.println(arr[1]);
+
+    Integer[] arr2 = new Integer[10];
+//    arr2={1,2,3,4,5,6,7,8,9,0}; // error
+    System.out.println(arr2);
+
+    Integer[] arr3 = {1,2,3,4,5,6,7,8,9,0};
+    System.out.println(arr3); // doesn't give the first element, but the memory hash code
+
+    Integer arr4[] = new Integer[3];
+    arr4[0] = 0;
+    arr4[1] = 1;
+    arr4[2] = 2;
+    System.out.println(arr4[1]);
+
+    // smaller boxes fit in bigger boxes
+//    int x = (long)10L; // invalid
+    long t = (long) ((int)10); // valid
+  }
+}
---
+++ src/main/java/org/example/PrimitiveDataTypes.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class PrimitiveDataTypes {
+  public static void main(String[] args) {
+    int i = 10;
+    System.out.println(i);
+
+    byte by = 21;
+    System.out.println(by);
+
+    short s = 14_3_21;
+    System.out.println(s);
+
+    float f = 34.51F;
+    System.out.println(f);
+
+    long l = 24_151_251L;
+    System.out.println(l);
+
+    double d = 214.511D;
+    System.out.println(d);
+
+    char c = 'a';
+    System.out.println(c);
+
+    boolean bo = true;
+    System.out.println(bo);
+  }
+}
